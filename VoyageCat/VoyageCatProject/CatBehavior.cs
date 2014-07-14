6ee9a848bb8f0b7175555d5667bd9dcb6715a40e
Brian Yang <brian@brianyang.me>
2014-07-15 00:12:25 +0100

    Add Cat Behavior for bullets

--- VoyageCat/VoyageCatProject/CatBehavior.cs
+++ VoyageCat/VoyageCatProject/CatBehavior.cs
@@ -18,19 +18,47 @@ namespace VoyageCatProject
         public static Vector2 LEFT = new Vector2(-1, 0);
         public static Vector2 RIGHT = new Vector2(1, 0);
 
-
         [RequiredComponent]
         public Transform2D transformation;
-        
+
+        private class SimpleGun
+        {
+            public int firingDelay;
+            public int currentDelay;
+            public SimpleGun(int firingDelay)
+            {
+                this.firingDelay = firingDelay;
+                currentDelay = firingDelay;
+            }
+            public bool CanFire()
+            {
+                return currentDelay >= firingDelay;              
+            }
+            public void Fire()
+            {
+                currentDelay = 0;
+            }
+            public void Update(TimeSpan gameTime)
+            {
+                if (currentDelay < firingDelay)
+                    currentDelay += gameTime.Milliseconds;
+            }
+        }
+
+        private SimpleGun gun;
+
         public CatBehavior()
             : base("cat")
         {
             this.transformation = null;
-
+            gun = new SimpleGun(800);
         }
 
         protected override void Update(TimeSpan gameTime)
         {
+            //Update gun
+            gun.Update(gameTime);
+
             //Prepare for input
             Vector2 direction = new Vector2(0, 0);
 
@@ -52,6 +80,14 @@ namespace VoyageCatProject
             {
                 direction += DOWN;
             }
+            if (keyboard.Space == ButtonState.Pressed)
+            {
+                if (gun.CanFire())
+                {
+                    gun.Fire();
+                    
+                }
+            }
 
             //Prepare target coordinate
             Vector2.Normalize(direction);
@@ -68,6 +104,10 @@ namespace VoyageCatProject
                 transformation.X = newX;
                 transformation.Y = newY;
             }
+
+            if (newX < 50)
+                EntityManager.Remove(this.Owner);
+
         }
     }
 }
