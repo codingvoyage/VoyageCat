5815e3c8b2b3fe686f85cf994eb7719b6399ef17
Edmund Qiu <edmundjqiu@gmail.com>
2014-07-14 19:03:46 -0400

    Cat supports firing a gun. Gun not implemented yet though.

--- VoyageCat/VoyageCatProject/CatBehavior.cs
+++ VoyageCat/VoyageCatProject/CatBehavior.cs
@@ -13,24 +13,52 @@ namespace VoyageCatProject
 {
     class CatBehavior : Behavior
     {
-        public static const Vector2 UP = new Vector2(0, -1);
-        public static const Vector2 DOWN = new Vector2(0, 1);
-        public static const Vector2 LEFT = new Vector2(-1, 0);
-        public static const Vector2 RIGHT = new Vector2(1, 0);
-
+        public static Vector2 UP = new Vector2(0, -1);
+        public static Vector2 DOWN = new Vector2(0, 1);
+        public static Vector2 LEFT = new Vector2(-1, 0);
+        public static Vector2 RIGHT = new Vector2(1, 0);
 
         [RequiredComponent]
         public Transform2D transformation;
-        
+
+        private class SimpleGun
+        {
+            public int firingDelay;
+            public int currentDelay;
+            public SimpleGun(int firingDelay)
+            {
+                this.firingDelay = firingDelay;
+                currentDelay = firingDelay;
+            }
+            public bool CanFire()
+            {
+                return currentDelay >= firingDelay;              
+            }
+            public void Fire()
+            {
+                currentDelay = 0;
+            }
+            public void Update(TimeSpan gameTime)
+            {
+                if (currentDelay < firingDelay)
+                    currentDelay += gameTime.Milliseconds;
+            }
+        }
+
+        private SimpleGun gun;
+
         public CatBehavior()
             : base("cat")
         {
             this.transformation = null;
-
+            gun = new SimpleGun(800);
         }
 
         protected override void Update(TimeSpan gameTime)
         {
+            //Update gun
+            gun.Update(gameTime);
+
             //Prepare for input
             Vector2 direction = new Vector2(0, 0);
 
@@ -52,6 +80,14 @@ namespace VoyageCatProject
             {
                 direction += DOWN;
             }
+            if (keyboard.Space == ButtonState.Pressed)
+            {
+                if (gun.CanFire())
+                {
+                    gun.Fire();
+                    
+                }
+            }
 
             //Prepare target coordinate
             Vector2.Normalize(direction);
@@ -68,6 +104,10 @@ namespace VoyageCatProject
                 transformation.X = newX;
                 transformation.Y = newY;
             }
+
+            if (newX < 50)
+                EntityManager.Remove(this.Owner);
+
         }
     }
 }
